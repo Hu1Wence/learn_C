4c85dba96b25cf992c0ff6bf1c4f17156b72edf8
xiaohutongxue <929125066@qq.com>
2019-08-27 23:20:10 +0800

    练习1

---
+++ 8.27/练习1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//把英寸单位转换为厘米单位（1英寸=2.54厘米）
+int main1()
+{
+    int x = 0;
+    printf("请用户输入英寸值：");
+    scanf("%d", &x);
+    printf("%d英寸 = %lf厘米\n", x, x*2.54);
+    
+    return 0;
+}
